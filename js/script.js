88e99a5f375f0d5feb33ef0b638e67188a3d3e8a
Tracey <radcliffe.tracey@gmail.com>
2016-05-12 16:11:21 -0700

    handlebars is working

--- js/script.js
+++ js/script.js
@@ -9,17 +9,21 @@ function Projects(o) {
 }
 
 Projects.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
+  // var $newProject = $('article.template').clone();
+  //
+  // $newProject.find('img').attr('src', this.img);
+  // $newProject.find('a').attr('href', this.projectUrl);
+  // $newProject.find('a').text(this.ttle);
+  // $newProject.find('p').attr('data-aboutProj', this.aboutProj).text(this.aboutProj);
+  //
+  // $newProject.removeClass('template');
+  // $newProject.attr('data-ttle', this.ttle);
+  //
+  // return $newProject;
+  var $source = $('#portfolio-template').html();
+  var template = Handlebars.compile($source);
+  return template(this);
 
-  $newProject.find('img').attr('src', this.img);
-  $newProject.find('a').attr('href', this.projectUrl);
-  $newProject.find('a').text(this.ttle);
-  $newProject.find('p').attr('data-aboutProj', this.aboutProj).text(this.aboutProj);
-
-  $newProject.removeClass('template');
-  $newProject.attr('data-ttle', this.ttle);
-
-  return $newProject;
 };
 
 myProjectsArry.forEach(function(el){

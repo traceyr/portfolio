30669b4f3a88144e728bd40f825da64c3e2f81a5
Tracey <radcliffe.tracey@gmail.com>
2016-05-19 18:02:14 -0700

    fixed linter issue

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function Projects(o) {
   Object.keys(o).forEach(function(prop, index, keys){
     this[prop] = o[prop];
@@ -20,7 +19,7 @@ Projects.loadAll = function(dataIn){
   });
 };
 
- (function() {
+(function() {
   if (localStorage.myData) {
     console.log('There is local storage.');
     Projects.loadAll(JSON.parse(localStorage.myData));

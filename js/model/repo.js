66defff432e65852a5a9150895ca741f5331436b
Tracey <radcliffe.tracey@gmail.com>
2016-06-08 20:41:02 -0700

    fixed the token call

--- js/model/repo.js
+++ js/model/repo.js
@@ -3,12 +3,16 @@
   randomData.all = [];
 
   randomData.requestData = function(callback) {
-    $.get('github/users/traceyr/repos' +
-    '?per_page=10' +
-    '&sort=updated')
-    .done(function(data){
-      randomData.all = data;
-    }).done(callback);
+    $.ajax({
+      url: 'https://api.github.com/users/' + gitRepo.gitUser + '/repos' + '?per_page=10' + '&fork=false' + '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + gitRepo.gitToken},
+      success: function(data, message, xhr){
+        console.log(data);
+        randomData.all = data;
+        callback();
+      }
+    });
   };
 
   randomData.with = function(attr) {
